01696918595180f35910f371709bca403eaa2840
Karim Pedemonte <81413113+KarimPedemonte@users.noreply.github.com>
2024-11-15 17:05:39 +0100

    Update generator.py

--- generator/ros2_devel/generator.py
+++ generator/ros2_devel/generator.py
@@ -90,7 +90,7 @@ class CodeGenAndROSUtils():
         return 'self.get_logger().info({0})\n'.format(text)
     
     def get_ros_time_line(self):
-        return 'float(str(self.get_clock().now().to_msg().sec) + "." + str(self.get_clock().now().to_msg().nanosec))'
+        return 'float(self.get_clock().now().to_msg().sec) + float((self.get_clock().now().to_msg().nanosec) / 1000000000)'
         # return '{"sec": int(self.get_clock().now().to_msg().sec), "nanosec": int(self.get_clock().now().to_msg().nanosec)}'
 
         
